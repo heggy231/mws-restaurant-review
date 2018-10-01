b89eba13258de7eb038eaeb8de9136c13b7aee8c
Heggyhere <heggyy@gmail.com>
2018-10-01 12:46:22 -0700

    browser sync server setup, auto refresh

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,4 +4,13 @@ import browserSync from 'browser-sync';
 const dev = console.log('are you gulping?');
 const server = browserSync.create();
 
+function serve(done) {
+  server.init({
+    server: {
+      baseDir: './',
+    },
+  });
+  done();
+}
+
 export default dev;
