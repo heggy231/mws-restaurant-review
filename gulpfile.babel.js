4be8c0864fba695a34f6e6183806f7d168b8d0f5
Heggyhere <heggyy@gmail.com>
2018-10-01 12:47:14 -0700

    reload function

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,11 @@ import browserSync from 'browser-sync';
 const dev = console.log('are you gulping?');
 const server = browserSync.create();
 
+function reload(done) {
+  server.reload();
+  done();
+}
+
 function serve(done) {
   server.init({
     server: {
